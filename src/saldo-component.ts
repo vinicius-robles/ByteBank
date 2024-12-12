bcdf354932c9189a55551f409b85cd14c1ee60a9
vinicius-robles <vinicius.robles@gmail.com>
2024-12-12 18:45:10 -0300

    Atualização na formatação de data para diferentes usos dentro da aplicação.

--- src/saldo-component.ts
+++ src/saldo-component.ts
@@ -13,5 +13,8 @@ if (elementoSaldo != null) {
 
 if (elementoDataAcesso != null) {
   const dataAcesso: Date = new Date();
-  elementoDataAcesso.textContent = formatarData(dataAcesso);
+  elementoDataAcesso.textContent = formatarData(
+    dataAcesso,
+    FormatoData.DIA_SEMANA_DIA_MES_ANO
+  );
 }
