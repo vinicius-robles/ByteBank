648841c1b940a5b1196108bc13cc8dd079dee754
vinicius-robles <vinicius.robles@gmail.com>
2024-12-13 17:02:54 -0300

    Refinamento da modularização e refatoração do código. Implementação da lógica para salvar as informações de operações no armazenamento local do navegador.

---
+++ dist/js/types/Conta.js
@@ -0,0 +1,47 @@
+import { TipoTransacao } from "./TipoTransacao.js";
+let saldo = 3000;
+const transacoes = JSON.parse(localStorage.getItem("transacoes"), (key, value) => {
+    if (key == "data") {
+        return new Date(value);
+    }
+    return value;
+}) || [];
+function debitar(valor) {
+    if (valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que zero!");
+    }
+    if (valor > saldo) {
+        throw new Error("Valor insuficiente!");
+    }
+    saldo -= valor;
+}
+function depositar(valor) {
+    if (valor <= 0) {
+        throw new Error("O valor a ser depositado deve ser maior que zero!");
+    }
+    saldo += valor;
+}
+const Conta = {
+    getSaldo() {
+        return saldo;
+    },
+    getDataAcesso() {
+        return new Date();
+    },
+    registrarTransacao(novaTransacao) {
+        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+            depositar(novaTransacao.valor);
+        }
+        else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
+            novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
+            debitar(novaTransacao.valor);
+        }
+        else {
+            throw new Error("Tipo de transação é inválido!");
+        }
+        transacoes.push(novaTransacao);
+        console.log(transacoes);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+    },
+};
+export default Conta;
---
+++ src/types/Conta.ts
@@ -0,0 +1,58 @@
+import { Transacao } from "./Transacao.js";
+import { TipoTransacao } from "./TipoTransacao.js";
+
+let saldo: number = 3000;
+const transacoes: Transacao[] =
+    JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
+        if (key == "data") {
+            return new Date(value);
+        }
+
+        return value;
+    }) || [];
+
+function debitar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("O valor a ser debitado deve ser maior que zero!");
+    }
+    if (valor > saldo) {
+        throw new Error("Valor insuficiente!");
+    }
+    saldo -= valor;
+}
+
+function depositar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("O valor a ser depositado deve ser maior que zero!");
+    }
+    saldo += valor;
+}
+
+const Conta = {
+    getSaldo() {
+        return saldo;
+    },
+
+    getDataAcesso(): Date {
+        return new Date();
+    },
+
+    registrarTransacao(novaTransacao: Transacao): void {
+        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+            depositar(novaTransacao.valor);
+        } else if (
+            novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
+            novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
+        ) {
+            debitar(novaTransacao.valor);
+        } else {
+            throw new Error("Tipo de transação é inválido!");
+        }
+
+        transacoes.push(novaTransacao);
+        console.log(transacoes);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+    },
+};
+
+export default Conta;
